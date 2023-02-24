4de50d9cffd3a53aee42e78ebbc3476997a90739
Aygün Bayır <turkoaygun@gmail.com>
2023-02-24 14:34:27 +0300

    Formik native html etiketleriyle işlendi

--- src/pages/Dashboard/Contact.js
+++ src/pages/Dashboard/Contact.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik } from 'formik';
 
 function Contact() {
   return (
@@ -25,16 +25,37 @@ function Contact() {
          }, 400);
        }}
      >
-       {({ isSubmitting }) => (
-         <Form>
-           <Field type="email" name="email" />
-           <ErrorMessage name="email" component="div" />
-           <Field type="password" name="password" />
-           <ErrorMessage name="password" component="div" />
+       {({
+         values,
+         errors,
+         touched,
+         handleChange,
+         handleBlur,
+         handleSubmit,
+         isSubmitting,
+         /* and other goodies */
+       }) => (
+         <form onSubmit={handleSubmit}>
+           <input
+             type="email"
+             name="email"
+             onChange={handleChange}
+             onBlur={handleBlur}
+             value={values.email}
+           />
+           {errors.email && touched.email && errors.email}
+           <input
+             type="password"
+             name="password"
+             onChange={handleChange}
+             onBlur={handleBlur}
+             value={values.password}
+           />
+           {errors.password && touched.password && errors.password}
            <button type="submit" disabled={isSubmitting}>
              Submit
            </button>
-         </Form>
+         </form>
        )}
      </Formik>
     </div>
